22eb50c94036d8e3717d5af84c37e187365ca5ac
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-12-29 20:30:56 -0800

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -32,6 +33,34 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Valid id using isValid
+  // 404 - send back empty send
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // findById
+  // success
+  // if todo - send it back
+  // if no todo - send back 404 with empty body
+  // error
+  // 400 - and send empty body back
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
